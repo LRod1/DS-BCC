798c6f5e10c7d5ae40a543739c1c2af241655f2e
LRod1 <41514567+LRod1@users.noreply.github.com>
2024-01-15 15:17:50 +0100

    1.0.3 Keycheck entfernt, da in 115

--- scripts/options_menu.js
+++ scripts/options_menu.js
@@ -3,7 +3,6 @@
 trusted_domains_save.addEventListener("click", savePrefs);
 bcc_check.addEventListener("click", savePrefs);
 auto_complete.addEventListener("click", savePrefs);
-key_check.addEventListener("click", savePrefs);
 bcc_limit.addEventListener("click", savePrefs);
 stats_clear.addEventListener("click", clear);
 loadPrefs();
@@ -20,7 +19,7 @@ async function loadPrefs() {
 		//2 von 3
 		getPref().then((prefs) => {
 			console.log(prefs);
-			//trusted_domains laden und als String für das Input-Feld aufbereiten
+			//trusted_domains laden und als String fï¿½r das Input-Feld aufbereiten
 			if (prefs.hasOwnProperty("domain_list")) {
 				let domain_string = "";
 				prefs.domain_list.forEach(entry => {
@@ -51,7 +50,6 @@ function setDefaultPrefs() {
 	let options = {
 		"bcc_check": true,
 		"auto_complete": true,
-		"key_check": false,
 		"bcc_limit": 4
 	};
 	setButtonsToTrue();
@@ -72,7 +70,6 @@ function savePrefs() {
 	let options = {
 		"bcc_check": bcc_check.checked,
 		"auto_complete": auto_complete.checked,
-		"key_check": key_check.checked,
 		"bcc_limit": bcc_limit.value,
 		"domain_list": domain_list
 	};
@@ -84,19 +81,17 @@ function savePrefs() {
 function setButtonsToTrue() {
 	bcc_check.checked = true;
 	auto_complete.checked = true;
-	key_check.checked = false;
 	bcc_limit.value = BCC_LIMIT;
 }
 
 function setButtonsToPrefs(prefs) {
 	bcc_check.checked = prefs.bcc_check;
 	auto_complete.checked = prefs.auto_complete;
-	key_check.checked = prefs.key_check;
 	if (prefs.hasOwnProperty("bcc_limit")) bcc_limit.value = prefs.bcc_limit;
 	else bcc_limit.value = BCC_LIMIT;
 }
 
-//Prüft die Liste der vertrauenswürdigen Domains auf korrektes Format
+//Prï¿½ft die Liste der vertrauenswï¿½rdigen Domains auf korrektes Format
 function checkTrustedDomains(val) {
 	let domain_list = new Array();
 	let domains = val.replace(" ", "");
@@ -109,7 +104,7 @@ function checkTrustedDomains(val) {
 }
 
 
-//Löscht Statistiken
+//Lï¿½scht Statistiken
 function clear() {
 	browser.storage.local.clear();
 	setButtonsToTrue();
@@ -126,8 +121,6 @@ function loadLocalTxt() {
 	bcc_descr.textContent = browser.i18n.getMessage("options_bcc_descr");
 	autocompl_head.textContent = browser.i18n.getMessage("options_autocompl_head");
 	autocompl_descr.textContent = browser.i18n.getMessage("options_autocompl_descr");
-	pgp_head.textContent = browser.i18n.getMessage("options_pgp_head");
-	pgp_descr.textContent = browser.i18n.getMessage("options_pgp_descr");
 	limit_head.textContent = browser.i18n.getMessage("options_limit_head");
 	limit_descr.textContent = browser.i18n.getMessage("options_limit_descr");
 	clear_local.textContent = browser.i18n.getMessage("options_delete_head");
