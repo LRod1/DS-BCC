34e4cc0763489351cd4900d8339d9f74887eee88
LRod1 <41514567+LRod1@users.noreply.github.com>
2024-01-26 10:12:45 +0100

    1.0.5: Delete old mails

--- scripts/options_menu.js
+++ scripts/options_menu.js
@@ -5,10 +5,27 @@ bcc_check.addEventListener("click", savePrefs);
 auto_complete.addEventListener("click", savePrefs);
 bcc_limit.addEventListener("click", savePrefs);
 stats_clear.addEventListener("click", clear);
+list_folders_button.addEventListener("click", listFolders);
 loadPrefs();
 loadLocalTxt();
+//listener, um Fehler beim Wechseln von Tabs zu vermeiden - listet ordner neu auf
+let tab;
+browser.tabs.onActivated.addListener(async (activeInfo) => {
+	let t = await browser.tabs.getCurrent();
+	let w = await browser.windows.getCurrent();
+	tab = {tabId: t["id"], windowId: w["id"]};
+	console.log(tab);
+	console.log(activeInfo);
+	if (activeInfo["tabId"] == tab["tabId"]) {
+		listFolders();
+		console.log("list");
+	}
+});
 
 const BCC_LIMIT = 4;
+const HGB_TIME = 6;
+let folders_list = new Array(); //für das Löschen alter Mails
+
 
 async function getPref() {
 	return await browser.storage.local.get();
@@ -77,7 +94,6 @@ function savePrefs() {
 	browser.storage.local.set(options);
 }
 
-
 function setButtonsToTrue() {
 	bcc_check.checked = true;
 	auto_complete.checked = true;
@@ -103,13 +119,150 @@ function checkTrustedDomains(val) {
 	return domain_list;
 }
 
-
 //L�scht Statistiken
 function clear() {
 	browser.storage.local.clear();
 	setButtonsToTrue();
 }
 
+
+//Löschen alter Mails
+//Ordner mit alten Mails auflisten
+async function listFolders() {
+	const listContainer = document.getElementById("list_container");
+	console.log(listContainer);
+	listContainer.innerHTML = "";
+	folders_list = new Array();
+
+	//Accounts abrufen
+	let acc = await browser.accounts.list();
+	acc.forEach(async (a) => {
+		let folders = new Array();
+		//Ornders der einzelnen Accounts abrufen
+		let f = await browser.folders.getSubFolders(a);
+		//Rekursiv Unterordner der einzelnen Accounts abrufen
+		f.forEach(async (f) => await getSubs(f, folders));
+		//Mails in den einzelnen Ordnern prüfen und ggf. zu folders hinzufügen und mit Index pro Account und Ordner versehen
+		folders.forEach(async (item) => await checkFolder(listContainer, item));
+	});
+	addButton(listContainer);
+}
+
+//Löschen-Button für listFolders
+function addButton(listContainer) {
+	const listItem = document.createElement("div");
+	listItem.innerHTML = `<button type="button" id="del_button" />${browser.i18n.getMessage("options_delete_selected_button")}</button>`;
+	listItem.innerHTML += `<p>${browser.i18n.getMessage("options_cut-off_date")}: ${getSixYearsAgo()}</p>`
+	listContainer.appendChild(listItem);
+	del_button.addEventListener("click", deleteMails);
+}
+
+//rekursiv subfolder hinzufügen
+async function getSubs(folder, folders) {
+	folders.push(folder);
+	folder["subFolders"].forEach((f) => getSubs(f, folders));
+}
+
+//Nachrichten im Ordner (item) auflisten und Eintrag samt Checkbox hinzufügen
+async function checkFolder(listContainer, item) {
+	let page = await browser.messages.list(item);
+	//Nachrichten zählen
+	let i = checkMails(page.messages);
+	while (page.id) {
+		page = await browser.messages.continueList(page.id);
+		i += checkMails(page.messages);
+	}
+	if (i == 0) return;
+	//hinzufügen, da > 0
+	//AccountId + index als Bezeichner für Checkbox
+	let indexConcat = item["accountId"] + item["path"];
+	const listItem = document.createElement("div");
+	listItem.innerHTML = `<input type="checkbox" id="checkbox${indexConcat}" /> ${item.path} - ${i} ${browser.i18n.getMessage("options_messages")}`;
+	listContainer.appendChild(listItem);
+	folders_list.push(new Object({ obj: item, in: indexConcat }));
+}
+
+//Check Mails auf Datum
+function checkMails(mails) {
+	const sixYearsAgo = getSixYearsAgo();
+	//Mails pro Ordner zählen, bei denen das Filterkriterium zutrifft
+	let j = 0;
+	mails.forEach((m) => {
+		if (checkMail(m, sixYearsAgo)) {
+			j++;
+		}
+	});
+	return j;
+}
+
+//Prüfung auf einzelne Mail
+function checkMail(m, sixYearsAgo) {
+	if (m.date <= sixYearsAgo) {
+		return true;
+	}
+	return false;
+}
+
+//Mails in ausgewählten Ordnern löschen
+async function deleteMails() {
+	const selectedItems = [];
+	const sixYearsAgo = getSixYearsAgo();
+
+	try {
+		//Markierte Ordner sammeln
+		folders_list.forEach((item) => {
+			const checkbox = document.getElementById(`checkbox${item.in}`);
+			if (checkbox.checked) {
+				selectedItems.push(item.obj);
+			}
+		});
+
+		//Mails in ausgewählten Ordnern löschen
+		selectedItems.forEach(async (item) => {
+			let del = new Array();
+
+			//Prüfung der Mails in den Ordnern auf Kriterium aus checkMail
+			let mails = await browser.messages.list(item);
+			mails.messages.forEach(async (mail) => {
+				if (checkMail(mail, sixYearsAgo)) del.push(mail.id);
+			});
+
+			while (mails.id) {
+				mails = await browser.messages.continueList(mails.id);
+				mails.messages.forEach(async (mail) => {
+					if (checkMail(mail, sixYearsAgo)) del.push(mail.id);
+				});
+			}
+
+			//false = papierkorb, true = vollständig löschen
+			let i = del.length;
+			await browser.messages.delete(del, false);
+			document.getElementById("list_status").innerHTML = browser.i18n.getMessage("options_moved_to_trash") + i + " " + browser.i18n.getMessage("options_messages");
+			listFolders();
+		});
+	} catch (err) {
+		document.getElementById("list_status").innerHTML = err.message;
+		listFolders();
+	}
+}
+
+//Löschdatum nach HGB ermitteln
+function getSixYearsAgo() {
+	// Aktuelles Datum
+	const currentDate = new Date();
+	// Berechne das Datum vor 1 Jahr
+	const oneYearAgo = new Date(currentDate);
+	oneYearAgo.setFullYear(currentDate.getFullYear() - 1);
+	oneYearAgo.setMonth(11); // Setze den Monat auf Dezember
+	oneYearAgo.setDate(31); // Setze den Tag auf den 31. Dezember
+	oneYearAgo.setHours(23);
+	oneYearAgo.setMinutes(59);
+	oneYearAgo.setSeconds(59);
+	// Berechne das Datum vor 6 Jahren ab dem 31. Dezember des Vorjahres
+	const sixYearsAgo = new Date(oneYearAgo);
+	sixYearsAgo.setFullYear(oneYearAgo.getFullYear() - HGB_TIME);
+	return sixYearsAgo;
+}
 
 //i18n
 function loadLocalTxt() {
@@ -125,4 +278,6 @@ function loadLocalTxt() {
 	limit_descr.textContent = browser.i18n.getMessage("options_limit_descr");
 	clear_local.textContent = browser.i18n.getMessage("options_delete_head");
 	stats_clear.textContent = browser.i18n.getMessage("options_delete_button");
+	list_folders.textContent = browser.i18n.getMessage("options_list_folders");
+	list_folders_button.textContent = browser.i18n.getMessage("options_list_folders_button");
 }
